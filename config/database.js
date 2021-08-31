e9fec02c75cd392af428e9fcc28e5b572cc1ea36
Peter Alexander <renormalize@gmail.com>
2021-08-31 23:55:39 +0530

    Update database.js to remove ssl

--- config/database.js
+++ config/database.js
@@ -4,12 +4,6 @@ require('dotenv').config();
 module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-	dialectOptions: {
-		ssl: {
-			require: true,
-			rejectUnauthorized: false
-		}
-	},
     pool: {
         max: 5,
         min: 0,
